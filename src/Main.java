196c880dcc9cb0bff42e4da2fd26e5ab9faf2023
Tomasz Babiak <contact@tomaszbabiak.pl>
2022-01-22 16:18:05 +0100

    cw4

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,35 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int liczba = scanner.nextInt();
+        // Indeks: 56103
 
         // Zadanie 1.a
-        switch (liczba % 2) {
-            case 0: System.out.println("Liczba jest parzysta"); break;
-            case 1: System.out.println("Liczba jest nieparzysta"); break;
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                System.out.println("Zadanie 1.a - BREAK");
+                break;
+            }
+
+            System.out.println("Zadanie 1.a - " + i);
         }
 
         // Zadanie 1.b
-        switch (liczba % 7) {
-            case 0: System.out.println("Liczba jest podzielna przez 7"); break;
-            default: System.out.println("Liczba nie jest podzielna przez 7"); break;
-        }
-
-        // Zadanie 2.a + 2.b
-        int[] ilosc = new int[] { 0, 0, 0, 0, 0 }; // 0 - o, 1 - p, 2 - x, 3 - z, 4 - q
-        int inne = 0;
-        String wyraz = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
-
-        for (int i = 0; i < wyraz.length(); i++) {
-            switch (wyraz.charAt(i)) {
-                case 'o': ilosc[0]++; break;
-                case 'p': ilosc[1]++; break;
-                case 'x': ilosc[2]++; break;
-                case 'z': ilosc[3]++; break;
-                case 'q': ilosc[4]++; break;
-                default: inne++; break;
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                System.out.println("Zadanie 1.b - CONTINUE");
+                continue;
             }
+
+            System.out.println("Zadanie 1.b - " + i);
         }
 
-        System.out.println("Litera 'o' wystąpiła: " + ilosc[0] + "x");
-        System.out.println("Litera 'p' wystąpiła: " + ilosc[1] + "x");
-        System.out.println("Litera 'x' wystąpiła: " + ilosc[2] + "x");
-        System.out.println("Litera 'z' wystąpiła: " + ilosc[3] + "x");
-        System.out.println("Litera 'q' wystąpiła: " + ilosc[4] + "x");
-        System.out.println("Pozostałych liter: " + inne);
+        // Zadanie 1.c
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                System.out.println("Zadanie 1.c - RETURN");
+                return;
+            }
+
+            System.out.println("Zadanie 1.c - " + i);
+        }
     }
 }
