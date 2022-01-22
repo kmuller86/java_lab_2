8f98893e56934b76b08ce982beefc6ed5bbbadc7
Tomasz Babiak <contact@tomaszbabiak.pl>
2022-01-22 16:14:39 +0100

    cw3

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,44 @@
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Indeks: 56103
+        Scanner scanner = new Scanner(System.in);
 
-        // Zadanie 1
-        for (int i = 100; i > 0; i--) {
-            if (i % 3 == 0) {
-                System.out.print(i + " ");
+        int liczba = scanner.nextInt();
+
+        // Zadanie 1.a
+        switch (liczba % 2) {
+            case 0: System.out.println("Liczba jest parzysta"); break;
+            case 1: System.out.println("Liczba jest nieparzysta"); break;
+        }
+
+        // Zadanie 1.b
+        switch (liczba % 7) {
+            case 0: System.out.println("Liczba jest podzielna przez 7"); break;
+            default: System.out.println("Liczba nie jest podzielna przez 7"); break;
+        }
+
+        // Zadanie 2.a + 2.b
+        int[] ilosc = new int[] { 0, 0, 0, 0, 0 }; // 0 - o, 1 - p, 2 - x, 3 - z, 4 - q
+        int inne = 0;
+        String wyraz = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+
+        for (int i = 0; i < wyraz.length(); i++) {
+            switch (wyraz.charAt(i)) {
+                case 'o': ilosc[0]++; break;
+                case 'p': ilosc[1]++; break;
+                case 'x': ilosc[2]++; break;
+                case 'z': ilosc[3]++; break;
+                case 'q': ilosc[4]++; break;
+                default: inne++; break;
             }
         }
 
-        System.out.println();
-
-        // Zadanie 2
-        int[] tab = new int[5];
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            tab[i] = scanner.nextInt();
-        }
-
-        for (int v : tab) {
-            System.out.print((v + 11) + " ");
-        }
+        System.out.println("Litera 'o' wystąpiła: " + ilosc[0] + "x");
+        System.out.println("Litera 'p' wystąpiła: " + ilosc[1] + "x");
+        System.out.println("Litera 'x' wystąpiła: " + ilosc[2] + "x");
+        System.out.println("Litera 'z' wystąpiła: " + ilosc[3] + "x");
+        System.out.println("Litera 'q' wystąpiła: " + ilosc[4] + "x");
+        System.out.println("Pozostałych liter: " + inne);
     }
 }
